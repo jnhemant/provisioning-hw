838f837ad9ffdca16f3a6688f6bee9364e76ea02
Hemant Jain <jnhemant319@gmail.com>
2021-10-22 13:15:28 +0530

    implementation for get provisions REST API

--- src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
+++ src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
@@ -1,5 +1,12 @@
 package com.voxloud.provisioning.controller;
 
+import com.voxloud.provisioning.service.ProvisioningService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class ProvisioningController {
 
+    @Autowired
+    private ProvisioningService provisioningService;
+
     // TODO Implement controller method
+    @GetMapping(value = "provisioning/{macAddress}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> getConfiguration(@PathVariable String macAddress){
+        String config = provisioningService.getProvisioningFile(macAddress);
+        if(config == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(config);
+    }
 }
--- src/main/java/com/voxloud/provisioning/service/ProvisioningServiceImpl.java
+++ src/main/java/com/voxloud/provisioning/service/ProvisioningServiceImpl.java
@@ -1,12 +1,90 @@
 package com.voxloud.provisioning.service;
 
+import com.voxloud.provisioning.entity.Device;
+import com.voxloud.provisioning.repository.DeviceRepository;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class ProvisioningServiceImpl implements ProvisioningService {
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @Value("${provisioning.domain}")
+    private String domain;
+
+    @Value("${provisioning.port}")
+    private String port;
+
+    @Value("${provisioning.codecs}")
+    private String codecs;
 
     public String getProvisioningFile(String macAddress) {
         // TODO Implement provisioning
+        Optional<Device> deviceConfig = deviceRepository.findById(macAddress);
+        if(!deviceConfig.isPresent()){
+            return null;
+        }
+
+        final String usernameAttribute = "username";
+        final String passwordAttribute = "password";
+        final String domainAttribute = "domain";
+        final String portAttribute = "port";
+        final String codecsAttribute = "codecs";
+
+        Device device = deviceConfig.get();
+
+        Map<String, Object> configurationMap = new HashMap<>();
+        configurationMap.put(usernameAttribute, device.getUsername());
+        configurationMap.put(passwordAttribute, device.getPassword());
+        configurationMap.put(domainAttribute, domain);
+        configurationMap.put(portAttribute, port);
+        configurationMap.put(codecsAttribute, codecs);
+
+        String deviceOverrideFragment = device.getOverrideFragment();
+
+        switch (device.getModel()){
+            case DESK:{
+                if(deviceOverrideFragment != null && !deviceOverrideFragment.equals("")){
+                    String[] overrideFragmentArray = deviceOverrideFragment.split("\\R+");
+                    if(overrideFragmentArray.length > 0){
+                        for (String overrideFragment : overrideFragmentArray) {
+                            String[] fragmentProperty = overrideFragment.split("=");
+                            if(fragmentProperty.length == 2){
+                                configurationMap.put(fragmentProperty[0], fragmentProperty[1]);
+                            }
+                        }
+                    }
+                }
+
+                StringBuilder configString = new StringBuilder();
+
+                configurationMap.forEach((key, value) ->
+                        configString.append(key).append(":").append(value).append(System.lineSeparator()));
+                return configString.toString().trim();
+            }
+            case CONFERENCE: {
+                if(deviceOverrideFragment != null && !deviceOverrideFragment.equals("")){
+                    JSONObject overrideFragmentJson = new JSONObject(deviceOverrideFragment);
+                    Map overrideFragmentMap = overrideFragmentJson.toMap();
+                    if(overrideFragmentMap != null && overrideFragmentMap.size() > 0){
+                        overrideFragmentMap.forEach((key, value) -> configurationMap.put((String) key, value));
+                    }
+                }
+                Object codecsConfiguration = configurationMap.get(codecsAttribute);
+                if(codecsConfiguration instanceof String){
+                    configurationMap.put(codecsAttribute, ((String) codecsConfiguration).split(","));
+                }
+                JSONObject configJson = new JSONObject(configurationMap);
+                return configJson.toString(3);
+            }
+        }
         return null;
     }
 }
